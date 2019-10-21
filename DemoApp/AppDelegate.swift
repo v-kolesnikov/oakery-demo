d180a613d00d53504d8b98b95e90db8a74ac6806
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2019-10-21 20:12:31 +0300

    #7 Get rid of storyboards

--- DemoApp/AppDelegate.swift
+++ DemoApp/AppDelegate.swift
@@ -3,4 +3,13 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    guard let window = self.window else { fatalError("No Window") }
+    window.rootViewController = ViewController()
+    window.makeKeyAndVisible()
+    return true
+  }
 }
--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -3,5 +3,11 @@ import UIKit
 class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
+    let touchMeButton = UIButton()
+    touchMeButton.backgroundColor = UIColor.lightGray
+    touchMeButton.center = view.center
+    touchMeButton.setTitle("Touch me", for: .normal)
+    view.addSubview(touchMeButton)
+    view.backgroundColor = .white
   }
 }
